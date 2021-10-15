26af95f60291769e3c025e86599deb260c053f6a
EnVIL-Yuri <jarod.marie50250@hotmail.fr>
2021-10-15 09:42:20 +0200

    modif_Rename

--- create_ou_frasur001.ps1
+++ create_ou_frasur001.ps1
@@ -1,31 +1,22 @@
-#Crée les users
-New-ADUser -Name "admin_jarod.FR" -GivenName "Jarod" -Surname "Marie" -SamAccountName "admin_jarod" -UserPrincipalName "admin_jarod@fr.ASURMarieJarod.local" -AccountPassword (ConvertTo-SecureString -AsPlainText "admin_jm1" -Force)  -Enable $true -ChangePasswordAtLogon $true
-New-ADUser -Name "admin_dtrump.FR" -GivenName "Donald" -Surname "Trump" -SamAccountName "admin_dtrump" -UserPrincipalName "admin_dtrump@fr.ASURMarieJarod.local" -AccountPassword (ConvertTo-SecureString -AsPlainText "admin_dt1" -Force) -Enable $true -ChangePasswordAtLogon $true
-Get-ADuser -Identity "admin_jarod.FR"
-Get-ADuser -Identity "admin_dtrump.FR"
+# Création de la OU Administration
+New-ADOrganizationalUnit -Name "Comptabilité"   -Path "DC=fr,DC=ASURMarieJarod,DC=local"
+New-ADOrganizationalUnit -Name "Ordinateurs"    -Path "DC=fr,DC=ASURMarieJarod,DC=local"
+New-ADOrganizationalUnit -Name "Administration" -Path "DC=fr,DC=ASURMarieJarod,DC=local"
+New-ADOrganizationalUnit -Name "Direction"      -Path "DC=fr,DC=ASURMarieJarod,DC=local"
+New-ADOrganizationalUnit -Name "Serveurs"       -Path "DC=fr,DC=ASURMarieJarod,DC=local"
+New-ADOrganizationalUnit -Name "Filers"         -Path "DC=fr,DC=ASURMarieJarod,DC=local"
 Pause
 
-#Crée des groupes
-New-ADGroup -Name "L3_admin" -GroupScope Global 
-New-ADGroup -Name "L1_admin" -GroupScope Global 
-
-Get-ADGroup -Identity L3_admin
-Get-ADGroup -Identity L1_admin
+# Permet de ne pas activer la protection anti supression de l'OU
+Get-ADOrganizationalUnit -filter "Name -eq 'Comtabilité'"    | Set-ADOrganizationalUnit  -ProtectedFromAccidentalDeletion $False
+Get-ADOrganizationalUnit -filter "Name -eq 'Ordinateurs'"    | Set-ADOrganizationalUnit  -ProtectedFromAccidentalDeletion $False
+Get-ADOrganizationalUnit -filter "Name -eq 'Administration'" | Set-ADOrganizationalUnit  -ProtectedFromAccidentalDeletion $False
+Get-ADOrganizationalUnit -filter "Name -eq 'Direction'"      | Set-ADOrganizationalUnit  -ProtectedFromAccidentalDeletion $False
+Get-ADOrganizationalUnit -filter "Name -eq 'Serveurs'"       | Set-ADOrganizationalUnit  -ProtectedFromAccidentalDeletion $False
+Get-ADOrganizationalUnit -filter "Name -eq 'Filers'"         | Set-ADOrganizationalUnit  -ProtectedFromAccidentalDeletion $False
 Pause
 
-#Déplacer les users vers les groupes atitré
-Add-ADGroupMember -Identity L3_admin -Members admin_jarod.FR
-Add-ADGroupMember -Identity L1_admin -Members admin_dtrump.FR
-Pause
-
-#Deplace le groupe L1_admin vers la OU administration
-Move-ADObject -Identity "CN=L1_admin,CN=Users,DC=fr,DC=ASURMarieJarod,DC=local" -TargetPath "OU=Administration,DC=fr,DC=ASURMarieJarod,DC=local"
-Move-ADObject -Identity "CN=L3_admin,CN=Users,DC=fr,DC=ASURMarieJarod,DC=local" -TargetPath "OU=Administration,DC=fr,DC=ASURMarieJarod,DC=local"
-Get-ADGroup -Identity L1_admin
-Get-ADGroup -Identity L3_admin
-Pause
-
-#Deplace le groupe L3_admin vers le groupe Admins du domaine
-Add-ADGroupMember -Identity "Admins du domaine" -Members L3_admin 
-Get-ADGroup -Identity L3_admin
+#Déplacer les OU Ordinateurs vers Comptabilité et Filers vers Serveurs
+Move-ADObject -Identity "OU=Ordinateurs,DC=fr,DC=ASURMarieJarod,DC=local" -TargetPath "OU=Comptabilité,DC=fr,DC=ASURMarieJarod,DC=local"
+Move-ADObject -Identity "OU=Filers,DC=fr,DC=ASURMarieJarod,DC=local" -TargetPath "OU=Serveurs,DC=fr,DC=ASURMarieJarod,DC=local"
 Pause
